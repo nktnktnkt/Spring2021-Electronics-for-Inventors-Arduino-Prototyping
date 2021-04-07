d53a5e4622acf824980098dfa244c72c572b80b7
Nolan Thomas <nolan.thomas@pcc.edu>
2021-04-06 20:44:34 -0700

    Update Blink.ino

--- Class Content & Resources/Week 1/Blink/Blink.ino
+++ Class Content & Resources/Week 1/Blink/Blink.ino
@@ -1,74 +1,37 @@
-//initialize the global variables
-int led = 13;
-int led2 = 12;
-int ledRed = 7;
-int ledGreen = 6;
-int ledBlue = 5;
-int modulo = 0;
+/*
+  Blink
 
-// the setup routine runs once when you press reset:
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledGreen, OUTPUT);
-  pinMode(ledBlue, OUTPUT);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
+// the loop function runs over and over again forever
 void loop() {
-  modulo = modulo + 1;
-  int delayValue = 5 * modulo;
-  if(delayValue > 100) {
-    modulo = 0;
-  }
-  
-  digitalWrite(led, HIGH);
-  digitalWrite(led2, HIGH);
-  delay(delayValue);
-  digitalWrite(led, LOW);
-  digitalWrite(led2, LOW);
-  delay(delayValue);
-  digitalWrite(led2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delayValue);               // wait for a second
-  digitalWrite(led2, LOW);    // turn the LED off by making the voltage LOW
-  delay(delayValue);  // wait for a second
-
-  digitalWrite(ledRed, HIGH);
-  delay(delayValue);
-  digitalWrite(ledRed, LOW);
-  delay(delayValue);
-  digitalWrite(ledGreen, HIGH);
-  delay(delayValue);
-  digitalWrite(ledGreen, LOW);
-  delay(delayValue);
-  digitalWrite(ledBlue, HIGH);
-  delay(delayValue);
-  digitalWrite(ledBlue, LOW);
-  delay(delayValue);
-
-  //purple
-  digitalWrite(ledBlue, HIGH);
-  digitalWrite(ledRed, HIGH);
-  delay(delayValue);
-  digitalWrite(ledBlue,LOW);
-  digitalWrite(ledRed, LOW);
-  delay(delayValue);
-
-  //yellow
-  digitalWrite(ledGreen, HIGH);
-  digitalWrite(ledRed, HIGH);
-  delay(delayValue);
-  digitalWrite(ledGreen,LOW);
-  digitalWrite(ledRed, LOW);
-  delay(delayValue);
-
-  //teal
-  digitalWrite(ledGreen, HIGH);
-  digitalWrite(ledBlue, HIGH);
-  delay(delayValue);
-  digitalWrite(ledGreen,LOW);
-  digitalWrite(ledBlue, LOW);
-  delay(delayValue);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
